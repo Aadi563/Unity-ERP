d582ca63f22cb47bd16c553d9b2c743fee178433
Aditya Jain <aadi.562003@gmail.com>
2025-09-21 15:32:03 +0530

    chore: initial commit with backend and frontend

---
+++ backend/src/main/java/com/unity/erp/UnityERP/UnityErpApplication.java
@@ -0,0 +1,13 @@
+package com.unity.erp.UnityERP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnityErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnityErpApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/unity/erp/UnityERP/UnityErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unity.erp.UnityERP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnityErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
